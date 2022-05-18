532d67617f92d4622ce83e9c88ad64add0c8784f
JustRayCB <92854970+JustRayCB@users.noreply.github.com>
2022-05-18 20:00:14 +0200

    save

--- attack.cpp
+++ attack.cpp
@@ -10,22 +10,21 @@
 #include <cctype>
 #include <vector>
 #include <tuple>
-#include "vigenere.cpp"
+
 
 using namespace std;
 vector<size_t> lines;
 
 int main(int argc, char *argv[]) {
-    /*if (argc < 4) {
+    if (argc < 4) {
         cout << "Utilisation: ./attack.cpp <nom_fichier_chiffre> <long_max_clé> <nom_fichier_dechiffre>" << endl;
         return 1;
-    }*/
+    }
+    string nomFichierChiffre = argv[1];
+    size_t realKey = size_t(stoi(argv[2]));
+    string nomFichierDechiffre = argv[3];
 
-    string nomFichierChiffre = "C:\\Users\\Craya\\CLionProjects\\real_ldp3\\chiffre\\chiffre_6.txt";
-    size_t realKey = 100;
-    string nomFichierDechiffre = "C:\\Users\\Craya\\CLionProjects\\real_ldp3\\dechiffre1.txt";
-
-    tuple<string,vector<size_t>> to_decrypt = read_file(nomFichierChiffre);
+    tuple<string, vector<size_t>> to_decrypt = read_file(nomFichierChiffre);
     lines = get<1>(to_decrypt);     // Global Variable that will contain the length of the line in the files
     attack(get<0>(to_decrypt), nomFichierDechiffre, realKey);
 }
