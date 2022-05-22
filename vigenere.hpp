3a09f418dddea7de477e7bf472c0b2c67d0d33d3
JustRayCB <92854970+JustRayCB@users.noreply.github.com>
2022-05-22 20:50:44 +0200

    delete useless functions

--- vigenere.hpp
+++ vigenere.hpp
@@ -28,17 +28,6 @@ struct Clef {
 };
 
 /**
- * Dechiffre un message chiffré encodé à l'aide du chiffre de Vigenère et
- * d'une clé connue.
- *
- * @param cypher Le message à déchiffrer
- * @param clef La clef
- * @param plain String qui contiendra le message déchiffré après l'appel
- * @return None
- */
-void decode(const std::string &cypher, struct Clef *clef, std::string &plain);
-
-/**
  * Etant donné le message chiffré, renvoi une clé de type struct
  * Clef (cfr. l'énoncé pour sa déclaration)
  *
@@ -83,43 +72,3 @@ size_t findOccurenceWord(const std::string &text, const std::string &word);
 
 std::string findRepeatedString(const std::string &text);
 
-//      ****Use to decrypt****
-
-
-/**
- * Function that read the file named filename and recover the lines and the length of these lines
- *
- * @param filename the name of the file
- * @return tuple with the result of all lines of the file as a string and a vector with the length of the lines' file
- */
-std::tuple<std::string, std::vector<size_t>> read_file(const std::string& filename);
-
-/**
- * Function that will associate a password according to the length of text and the spaces in there
- * @param text the string which was in the fil to decrypt
- * @param pw Password to associate
- * @return the associated password as a string
- */
-std::string associate_pw(const std::string& text, const std::string& pw);
-
-
-
-void writeFile(const std::string &text, const struct Clef *clef, const std::string &filename, const std::vector<size_t> &length);
-
-/**
- * Decrypt the text with the password
- *
- * @param text encrypted text to decrypt
- * @param mdp Password
- * @return string decrypted
- */
-std::string decrypt(const std::string &text, const std::string &mdp);
-
-/**
- * Will write a text  line per line in a file
- *
- * @param filename File where we want to write the text
- * @param text Text we want to write in the file
- * @param length List of the line's length to write in the file
- */
-void write_file(const std::string& filename, std::string text, const std::vector<size_t>& length);
